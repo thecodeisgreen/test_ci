25a2b84187a8126b2f784be1b728677452c64d9a
Hubert Bettan <hubert.bettan@gmail.com>
2019-10-22 07:47:26 +0200

    fix path issue

--- test_ci.go
+++ test_ci.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"test_ci/models/counter"
 )
 
 func F1() string {
@@ -15,11 +14,6 @@ func F2() string {
 
 func F3() string {
 	return "F3"
-}
-
-func F4() int {
-	counter.Inc()
-	counter.Inc()
 }
 
 func main() {
